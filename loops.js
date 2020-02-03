3c9891c2ea373b1c6c9431885228e1bd9b34a88a
happy-clone-7156 <kaidornel@gmail.com>
2020-02-03 19:43:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,8 +21,17 @@ function whileLoop(count){
 
 function doWhileLoop(num){
   
-  
-  
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+  return num
 }
 
 
